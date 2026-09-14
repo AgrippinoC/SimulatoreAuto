615aa8f502914e01ba86aa98e2c878e01c4ea80a
AgrippinoC <128165350+AgrippinoC@users.noreply.github.com>
2026-09-14 16:10:32 +0200

    Add files via upload

--- auto.sql
+++ auto.sql
@@ -14,14 +14,15 @@ CREATE TABLE veicoli (
     differenziale DOUBLE,
     rapp_max DOUBLE,
     rapp_cambio DOUBLE,
-    pot_max DOUBLE
+    pot_max DOUBLE,
+    consuP DOUBLE
 );
 
-INSERT INTO veicoli (nome, peso, coppia, raggio_ruota, m1, m2, m3, m4, m5, differenziale, rapp_max, rapp_cambio, pot_max) 
-                    VALUES ("Fiat Panda", 920.0, 92.0, 0.32, 3.5, 2.1, 1.4, 1.0, 0.8, 3.7, 6500.0, 5500.0, 55000.0),
-                           ("VW Golf", 1250.0, 200.0, 0.33, 3.8, 2.0, 1.3, 0.9, 0.7, 3.9, 6000.0, 5000.0, 81000.0),
-                           ("AlfaRomeo Giulietta", 1365.0, 250.0, 0.3, 3.9, 2.1, 1.4, 1.1, 0.9, 3.5, 6500.0, 5500.0, 125000.0),
-                           ("BMW M3", 1655.0, 400.0, 0.3, 4.0, 2.4, 1.5, 1.2, 1.0, 3.8, 8400.0, 8300.0, 309000.0);
+INSERT INTO veicoli (nome, peso, coppia, raggio_ruota, m1, m2, m3, m4, m5, differenziale, rapp_max, rapp_cambio, pot_max, consuP) 
+                    VALUES ("Fiat Panda", 920.0, 92.0, 0.32, 3.5, 2.1, 1.4, 1.0, 0.8, 3.7, 6500.0, 5500.0, 55000.0, 0.0025),
+                           ("VW Golf", 1250.0, 200.0, 0.33, 3.8, 2.0, 1.3, 0.9, 0.7, 3.9, 6000.0, 5000.0, 81000.0, 0.0035),
+                           ("AlfaRomeo Giulietta", 1365.0, 250.0, 0.3, 3.9, 2.1, 1.4, 1.1, 0.9, 3.5, 6500.0, 5500.0, 125000.0, 0.0045),
+                           ("BMW M3", 1655.0, 400.0, 0.3, 4.0, 2.4, 1.5, 1.2, 1.0, 3.8, 8400.0, 8300.0, 309000.0, 0.0050);
 
 CREATE TABLE piste (
     id INT PRIMARY KEY AUTO_INCREMENT,
